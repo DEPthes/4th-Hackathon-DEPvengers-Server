77aea2d1c56d7f70e4d60d1b0029659cda41d919
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-06-26 16:14:19 +0900

    [CHORE] 프로젝트 기본 세팅

---
+++ demo/src/main/java/OneMoment/demo/OneMomentApplication.java
@@ -0,0 +1,13 @@
+package OneMoment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMomentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneMomentApplication.class, args);
+	}
+
+}
